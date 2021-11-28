360d63f2cd3b0a72bba628a28893910e0769ae05
Konstantin Efimov <ekadesign@bk.ru>
2021-11-28 22:41:55 +0300

    refactor auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,40 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\Auth\InvalidCredentialsException;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request): JsonResponse
     {
-        $data = $request->all();
         $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
         ]);
         return response()->json(['user' => $user, 'token' => $user->createToken('apiToken')->plainTextToken], 201);
     }
 
-    public function login(Request $request)
+    /**
+     * @throws InvalidCredentialsException
+     */
+    public function login(LoginRequest $request): JsonResponse
     {
-        $data = $request->all();
-        $user = User::where('email', $data['email'])->first();
+        $user = User::where('email', $request->get('email'))->first();
 
-        if ($user && Hash::check($data['password'], $user->password)) {
-            $token = $user->createToken('apiToken')->plainTextToken;
-        } else {
-            return response()->json(['message' => 'Bad credentials'], 401);
+        if (!Hash::check($request->get('password'), $user->password)) {
+            throw new InvalidCredentialsException('Bad credentials', 401);
         }
 
+        $token = $user->createToken('apiToken')->plainTextToken;
         return response()->json(['user' => $user, 'token' => $token], 201);
     }
 
-    public function logout(Request $request)
+    public function logout(Request $request): JsonResponse
     {
         $request->user()->tokens()->delete();
-        return ['message' => 'Logged out'];
+        return response()->json([]);
     }
+
+    //@TODO add auth v2 with standartized responses. May be extract logic into requests here
 }
